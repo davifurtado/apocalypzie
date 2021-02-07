e2959df90cf0b519aa0cadf91400bebe5ebebd53
Davi Furtado <44845457+davifurtado@users.noreply.github.com>
2021-02-06 22:19:46 -0300

    created initial files for apocalypZie

---
+++ src/containers/RootContainer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const RootContainer = () => {
+    return (
+        <div>
+            <h1>Welcome to the ApocalypZie</h1>
+        </div>
+    )
+}
+
+export default RootContainer
