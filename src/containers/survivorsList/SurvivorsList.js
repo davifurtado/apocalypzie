1ee223a8a0780e9fc21244469fb457428564d6c7
Davi Furtado <44845457+davifurtado@users.noreply.github.com>
2021-02-07 11:50:05 -0300

    created filter for survivors in survivors list

--- src/containers/survivorsList/SurvivorsList.js
+++ src/containers/survivorsList/SurvivorsList.js
@@ -1,20 +1,29 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { SurvivorContext } from '../../contexts/SurvivorContext';
 import SurvivorCard from '../../components/survivorCard/SurvivorCard';
 import './survivors-list.css';
 
 const SurvivorList = () => {
     const { survivors } = useContext(SurvivorContext);
-            
+    const [filter, setFilter] = useState('')
     return (
-        <div className="survivors-list-container">
-            {
-                survivors.map((obj) => {
-                    return (
-                        <SurvivorCard survivor={obj} />
-                    )
-                })
-            }
+        <div>
+            <div className="survivors-list-filter">
+                <input 
+                    type="text"
+                    placeholder="Filter by name..."
+                    onChange={(e) => setFilter(e.target.value)}
+                />
+            </div>
+            <div className="survivors-list-container">
+                {
+                    survivors.filter(e => filter ? e.name?.toUpperCase().includes(filter.toUpperCase()) : true).map((obj) => {
+                        return (
+                            <SurvivorCard survivor={obj} />
+                        )
+                    })
+                }
+            </div>
         </div>
     )
 }
