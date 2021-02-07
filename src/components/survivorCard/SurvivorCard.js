6afe45e7017c959c7ce9145ea0b5b0c4fd2ae4d8
Davi Furtado <44845457+davifurtado@users.noreply.github.com>
2021-02-07 11:59:48 -0300

    added react bootstrap icons, created basic infected button on survivors

--- src/components/survivorCard/SurvivorCard.js
+++ src/components/survivorCard/SurvivorCard.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import './survivor-card.css';
 import avatar from '../../assets/survivor_avatar_placeholder.jpg';
+import { PersonXFill } from 'react-bootstrap-icons';
 
 const SurvivorCard = ({ survivor }) => {
     const [imageLoadError, setImageLoadError] = useState(true)
@@ -24,6 +25,11 @@ const SurvivorCard = ({ survivor }) => {
                 <div >Name: {survivor?.name}</div>
                 <div >Job: {survivor?.job}</div>
                 <div >Infected: {survivor?.infected ? 'Yes, better run' : "Nah, it's fine"}</div>
+                <div>
+                    <button>
+                        <PersonXFill />
+                    </button>
+                </div>
             </div>
         </div>
     );
