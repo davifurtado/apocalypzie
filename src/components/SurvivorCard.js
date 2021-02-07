4489f610d5913c0c78929287066f789ee9c42ea6
Davi Furtado <44845457+davifurtado@users.noreply.github.com>
2021-02-06 23:20:32 -0300

    created survivorCard with survivor name, job and infection

---
+++ src/components/SurvivorCard.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const SurvivorCard = ({ survivor }) => {
+    return (
+        <div>
+            {/* <div>
+                <img
+                    src={survivor.photo}
+                    alt="survivor"
+                    onError={e => {
+                        if (imageLoadError) {
+                            setImageLoadError(false)
+                            e.target.src = 'https://images.pexels.com/photos/3697676/pexels-photo-3697676.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500';
+                        }
+                    }}
+                    width="100%"
+                    height="100%"
+                />
+            </div> */}
+            <div>
+                <div >Name: {survivor?.name}</div>
+                <div >Job: {survivor?.job}</div>
+                <div >Infected: {survivor?.infected ? 'Yes, better run' : "Nah, it's fine"}</div>
+            </div>
+        </div>
+    );
+}
+
+export default SurvivorCard
